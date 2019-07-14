24c5d53cb255b072f94ebe56fee6b21ac39f9160
falfalaen <falfalaen@gmail.com>
2019-07-14 14:07:46 +0300

    creating myOwnComponent folder and components and declarating ti at app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// my created custom components
 import { MyComponent } from './my/my.component';
+import { MyOwnComponent } from './myOwn/myOwn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent
+    MyComponent,
+    MyOwnComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/myOwn/myOwn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-myOwn',
+  templateUrl: './myOwn.component.html',
+  styleUrls: ['./myOwn.component.css']
+})
+export class MyOwnComponent {
+
+}
