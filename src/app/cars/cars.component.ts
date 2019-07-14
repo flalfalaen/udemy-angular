488623b958b696745398d8921803f612c5b5c2ff
falfalaen <falfalaen@gmail.com>
2019-07-14 20:52:26 +0300

    html template cooperate with component.ts var1 event: Event as HtmlInputElement

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,12 +9,45 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
-  canAddCar = false;
+  // canAddCar = false; //lesson 2.6
 
+  addCarStatus = '';
+  inputText = '';
   constructor() {
-    setTimeout(() => {
-      this.canAddCar = true;
-    }, 4000);
+
   }
+
+  addCar() {
+    this.addCarStatus = 'Car added';
+  }
+
+  // 2.8 var3.2
+  // onKeyUp(event) {
+  //   console.log(event);
+  //   this.inputText = event.target.value;
+  // }
+  // 2.8 var3.1
+  // onKeyUp(event) {
+  //   console.log(event);
+  //   if (event.code === 'Enter') {
+  //     this.inputText = event.target.value;
+  //   }
+  // }
+  // 2.8 var2
+  // onKeyUp(value) {
+  //   console.log(value);
+  //   this.inputText = value;
+  // }
+    // 2.8 var1
+  onKeyUp(event: Event) {
+    console.log(event);
+    this.inputText = (event.target as HTMLInputElement).value;
+  }
+        // lesson 2.6
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.canAddCar = true;
+  //   }, 4000);
+  // }
 
 }
