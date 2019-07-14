d257203e27798c01b94a474b9725180e26388e25
falfalaen <falfalaen@gmail.com>
2019-07-14 14:43:56 +0300

    deleting my custom components folder for creating more flexible components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 // my created custom components
-import { MyComponent } from './my/my.component';
-import { MyOwnComponent } from './myOwn/myOwn.component';
-import { My2Component } from './my2/my2.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyComponent,
-    MyOwnComponent,
-    My2Component
+    AppComponent
+
   ],
   imports: [
     BrowserModule
