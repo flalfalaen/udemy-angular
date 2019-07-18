c09dfc6646af2b4d8c1dde753e9ff639660c3f15
falfalaen <falfalaen@gmail.com>
2019-07-18 20:16:33 +0300

    4.21 adding style to element with Renderer2

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,14 +1,16 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 
-export class BackgroundDirective implements OnInit{
-  constructor(private element: ElementRef) {
+export class BackgroundDirective implements OnInit {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
 
   }
   ngOnInit(): void {
-   this.element.nativeElement.style.backgroundColor = 'grey';
+    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+    // this.renderer.addClass(this.element.nativeElement, 'white-text');
+    // this.element.nativeElement.style.backgroundColor = 'grey';
   }
 }
