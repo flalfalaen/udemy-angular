5038a459d9f9880b110c186d17fc3adadf5369ab
falfalaen <falfalaen@gmail.com>
2019-07-18 20:42:59 +0300

    4.22 using decorator @HostBinding to bind whatever we want style or settings of our element and write less code

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, OnInit, Renderer2, HostListener} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 
 export class BackgroundDirective implements OnInit {
+
+  @HostBinding('style.backgroundColor') background: string;
+
   constructor(private element: ElementRef, private renderer: Renderer2) {
 
   }
@@ -17,11 +20,13 @@ export class BackgroundDirective implements OnInit {
   //   console.log(event);
   // }
   @HostListener('mouseenter') mouseEnter() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
-    this.renderer.addClass(this.element.nativeElement, 'white-text');
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+    // this.renderer.addClass(this.element.nativeElement, 'white-text');
+    this.background = 'red';
   }
   @HostListener('mouseleave') mouseLeave() {
-    this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
-    this.renderer.removeClass(this.element.nativeElement, 'white-text');
+    // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
+    // this.renderer.removeClass(this.element.nativeElement, 'white-text');
+    this.background = 'transparent';
   }
 }
