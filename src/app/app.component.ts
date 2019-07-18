16a4c789167569d8fad6b843d009dbc1294c2774
falfalaen <falfalaen@gmail.com>
2019-07-18 21:53:25 +0300

    4.24 using ngSwitch at ...component.html with sending parameters to ...component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   title = 'udemy-angular';
 
   headerText = 'Создание собственной диррективы';
+  items = [1, 2, 3, 4, 5];
+  current = 1;
+
+  onClick(switchedNumber: number) {
+    this.current = switchedNumber;
+  }
 }
